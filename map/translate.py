01985cb591a985489aebdc288a0601b79d3a767b
Unknown <edwardagarwala@gmail.com>
2016-09-22 21:34:11 -0400

    Added Code to translate the base message
    Translate converts a string using incrementLetter.
    Increment letter increases lower cases letters by n, with wrap around. All other character remain unchanged.

---
+++ map/translate.py
@@ -0,0 +1,18 @@
+print ord('a')
+
+
+inString = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+
+def incrementLetter(letter, n):
+    if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
+        return chr((ord(letter) - 97 + n) % 26 + 97)
+    else:
+        return letter
+
+
+def translate(inString, n):
+    print ''.join([incrementLetter(x, n) for x in inString])
+
+
+translate(inString, 2)
